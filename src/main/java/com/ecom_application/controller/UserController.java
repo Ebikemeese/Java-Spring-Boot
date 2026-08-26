43c5dafc239ae147990180a69e1e2c7459e570c5
Ebikemeese <ebikemeese257@gmail.com>
2026-08-26 19:01:26 +0100

    add controller, service and model folders

---
+++ src/main/java/com/ecom_application/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.ecom_application.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecom_application.model.User;
+import com.ecom_application.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/api/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        // return ResponseEntity.ok(userService.fetchAllUsers());
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/users/{id}")
+        public ResponseEntity<User> getUserById(@PathVariable Long id) {
+            // User user = userService.fetchUserById(id);
+            // if (user == null) {
+            //     return ResponseEntity.notFound().build();
+            // }
+            // return ResponseEntity.ok(user);
+
+            return userService.fetchUserById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        }
+
+    @PostMapping("/api/users")
+    public ResponseEntity<String> creatUsers(@RequestBody User user) {
+        userService.addUser(user);
+        // return ResponseEntity.ok("User added successfully");
+        return new ResponseEntity<>("User added successfully", HttpStatus.CREATED);
+    }
+
+    
+}
---
+++ src/main/java/com/ecom_application/service/UserService.java
@@ -0,0 +1,29 @@
+package com.ecom_application.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.ecom_application.model.User;
+
+@Service
+public class UserService {
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<User> fetchAllUsers() {
+        return userList;
+    }
+
+    public Optional<User> fetchUserById(Long id) {
+        return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
+    public List<User> addUser(User user) {
+        user.setId(nextId++);
+        userList.add(user);
+        return userList;
+    }
+}
